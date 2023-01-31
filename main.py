35e8500e16271b374e500f502802e3676db5bdba
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-01-31 17:24:00 +0700

    fixed ngrok

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import uvicorn
 from pyngrok import ngrok
 app = interface.app
 if __name__ == '__main__':
-    port = 5000
+    port = 3000
     # Open a ngrok tunnel to the HTTP server
     ngrok.set_auth_token(r"2L5F22vHF8HA7VDq8WmEpDGCroQ_wxBar5S3o5cywLSBuGmt")
     public_url = ngrok.connect(port).public_url
