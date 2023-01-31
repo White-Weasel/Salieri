a3f904158b5983ada32bf3fc127df05c73a336ce
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-01-31 16:51:35 +0700

    initial brain

--- interface/__init__.py
+++ interface/__init__.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import brain
 
 app = FastAPI()
-
+brain = brain.Brain()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/conversation")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.post("/conversation")
+async def root(request: Request):
+    res = await request.json()
+    return {
+        "message": brain.language(res['message']),
+    }
