743ee56ae594e0cf29e86f54c79c6b9791f370e1
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-01-31 17:31:20 +0700

    fixed ngrok

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = interface.app
 if __name__ == '__main__':
     port = 3000
     # Open a ngrok tunnel to the HTTP server
-    ngrok.set_auth_token(r"2L5VWLGxOLhsjQQeeEnnHPC6jAz_515XdsUJ2MMQ8Eivq8uJD")
+    ngrok.set_auth_token(r"2L5VkJxaX3C9C8bN3HjCe5K7YC5_71Sci5vUXcsyfY2ndJJTg")
     public_url = ngrok.connect(port)
     print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
     uvicorn.run("main:app", reload=True, port=port)
