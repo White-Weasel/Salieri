78a835816d36d982ae81102b004bb1e061aa7d89
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-01-31 16:57:12 +0700

    update main.py to work on colab

--- main.py
+++ main.py
@@ -2,4 +2,9 @@ import interface
 import uvicorn
 app = interface.app
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True, port=3000)
+    port = 5000
+    # Open a ngrok tunnel to the HTTP server
+    ngrok.set_auth_token(r"2L5F22vHF8HA7VDq8WmEpDGCroQ_wxBar5S3o5cywLSBuGmt")
+    public_url = ngrok.connect(port).public_url
+    print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}\"".format(public_url, port))
+    uvicorn.run("main:app", reload=True, port=port)
