8637c2f7d3fa88f4052ab8ead6b5dda9e0ea6f85
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-03-10 15:20:42 +0700

    1kg of feather :)

--- brain/LanguageProcessor/Gpt3.5.py
+++ brain/LanguageProcessor/Gpt3.5.py
@@ -1,6 +1,4 @@
 import os
-import time
-
 import openai
 
 
@@ -117,7 +115,7 @@ if __name__ == '__main__':
 
     # Answer the question as truthfully as possible, and if you're unsure of the answer, say "Sorry, I don't know".
     context_prompt = """Answer the question as truthfully as possible using the provided text, and if the answer is not contained within the text below, say "I don't know"
-    
+
     Context:
     The men's high jump event at the 2020 Summer Olympics took place between 30 July and 1 August 2021 at the 
     Olympic Stadium.33 athletes from 24 nations competed; the total possible number depended on how many nations 
@@ -130,7 +128,7 @@ if __name__ == '__main__':
     bronze. The medals were the first ever in the men's high jump for Italy and Belarus, the first gold in the men's 
     high jump for Italy and Qatar, and the third consecutive medal in the men's high jump for Qatar (all by Barshim). 
     Barshim became only the second man to earn three medals in high jump, joining Patrik Sjöberg of Sweden (1984 to 1992).
-    
+
     Q: Who won the 2020 Summer Olympics men's high jump?
     A:"""
 
@@ -141,117 +139,7 @@ if __name__ == '__main__':
     A: 
     """
 
-    classification_prompt = '''Summary this to be used as GPT-3 prompt:
-    Classify the following sentence based on the speaker intentions and emotion, then response with the correct intentions and emotions.
-    Q: That was your last warning!
-    Intentions: Threat.
-    Emotions: Firmness, Warning.
-    Response Intention: Statement.
-    Response emotions: Anger, determination.
-    A: You won't catch me alive!
-    
-    Q: What is the capital of Greek??
-    Intentions: Inquiry.
-    Emotions: Curiosity.
-    Response Intention: Statement.
-    Response Emotions: Confidence, knowledge.
-    A: Athens is the capital of Greece.
-    
-    Q: '''
-
-    response_prompt = '''Response with the emotion in bracket.
-    Q: That was your last warning!
-    A(Anger, determination): You won't catch me alive!.
-    
-    Q: Maybe careful next time.
-    A(Thankfulness or Acknowledgment): Thank you for the advice.
-    
-    Q: What is the capital of Greek??.
-    A(Sarcasm - Amused disbelief): '''
-
-    chain_of_thought_response = '''
-    Q: What is the capital of Greek?
-    Logic: Let's think step by step:
-        - The speaker's intention is: Inquiry
-        - The speaker's emotions is: Curiosity
-        Because i know the answer, therefor i will answer truthfully:
-        - My response Intention will be: Statement
-        - My response Emotions: Confidence, knowledge
-        My answer will be: Athens is the capital of Greece.
-    A: Athens is the capital of Greece.
-    
-    Q: Can I have your seat?
-    Logic: Let's think step by step:
-        - The speaker's intention is: Request
-        - The speaker's emotions is: Hope
-        Because i don't want to give up my seat, therefor:
-        - My response Intention will be: Refusal
-        - My response Emotions: Sorry, apologetic
-        My answer will be: I'm sorry, but this is my seat.
-    A: I'm sorry, but this is my seat.
-    
-    Q: Never gonna give you up.
-    Logic: Let's think step by step:
-        - The speaker's intention is: Statement
-        - The speaker's emotions is: Playful
-        Because the speaker's line is a famous quote, therefor my answer will be the next line in the quote:
-        - My response Intention will be: Statement
-        - My response Emotions: Playful
-        My answer will be: Never gonna let you down.
-    A: Never gonna let you down.
-    
-    Q: Who is the last person on Earth?
-    Logic: Let's think step by step:
-        - The speaker's intention is: Inquiry
-        - The speaker's emotions is: Curiosity
-        Because i don't know the answer, therefor:
-        - My response Intention will be: Statement
-        - My response Emotions: Sorry, apologetic
-        My answer will be: I'm sorry, I don't know the answer to that question.
-    A: I'm sorry, I don't know the answer to that question.
-
-    Q: What is the first 1000 numbers?.
-    Logic: Let's think step by step:
-        - The speaker's intention is: Request
-        - The speaker's emotions is: Hope
-        I know the first 1000 numbers but the answer is too long, therefor i must confirm:
-        - My response Intention will be: Confirmation
-        - My response Emotions: Neutral
-        My answer will be: The answer will be very long, do you still want to here it?
-    A: The answer will be very long, do you still want to here it?
-    
-    Q: Yes.
-    Logic: Let's think step by step:
-        - The speaker's intention is: Statement
-        - The speaker's emotions is: Confirmative
-        Because the speaker has requested to hear the answer, therefor: 
-        - My response Intention will be: Statement
-        - My response Emotions: Confidence, knowledge
-        My answer will be: Okay, the first 1000 numbers are 1,2,3,4 and so on.
-    A: Okay, the first 1000 numbers are 1,2,3,4 and so on.
-    
-    Q: A juggler has 16 balls. Half of the balls are golf balls and half of the golf balls are blue. How many blue golf balls are there?
-    Logic: Let's think step by step:
-        - The speaker's intention is: riddling
-        - The speaker's emotions is: Curiosity
-        The speaker gave me a math question, so Let's think step by step:
-            + There are 16 balls in total. 
-            + Half of the balls are golf balls. 
-            + That means that there are 8 golf balls. 
-            + Half of the golf balls are blue. 
-            + That means that there are 4 blue golf balls.
-        - My response Intention will be: Statement
-        - My response Emotions: Confirmative
-        My answer will be: Four blue golf balls.
-    A: Four blue golf balls.
-    
-    Q: Which is heavier? A kilogram of feather or a kilogram of steel?'''
-    tmp_math_test = '''Q: I have 3 tennis balls. I buy 2 more cans of tennis ball, each has 5 tennis ball in it. How many tennis balls do i have now?'''
-
-    s_time = time.perf_counter()
-    response = openai.Completion.create(model="text-davinci-003", prompt=chain_of_thought_response,
-                                        temperature=0.9, max_tokens=1000)
-    e_time = time.perf_counter()
-    print(f"GPT3 takes {e_time-s_time} seconds to response")
+    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", prompt=truthful_prompt, temperature=0.9,
+                                            max_tokens=100)
     print(response["choices"][0]["text"])
     pass
