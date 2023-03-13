0ace8292c5e0f6cc7d28fe0549c9d7e8b6523491
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-03-13 16:25:57 +0700

    added token counter.

--- brain/LanguageProcessor/Gpt3.5.py
+++ brain/LanguageProcessor/Gpt3.5.py
@@ -1,5 +1,30 @@
 import os
 import openai
+import tiktoken
+
+MODEL = 'gpt-3.5-turbo-0301'
+
+
+def num_tokens_from_messages(messages, model=MODEL):
+    """Returns the number of tokens used by a list of messages."""
+    try:
+        encoding = tiktoken.encoding_for_model(model)
+    except KeyError:
+        encoding = tiktoken.get_encoding("cl100k_base")
+    if model == "gpt-3.5-turbo-0301":  # note: future models may deviate from this
+        num_tokens = 0
+        for m in messages:
+            num_tokens += 4  # every message follows <im_start>{role/name}\n{content}<im_end>\n
+            for key, value in m.items():
+                num_tokens += len(encoding.encode(value))
+                if key == "name":  # if there's a name, the role is omitted
+                    num_tokens += -1  # role is always required and always 1 token
+        num_tokens += 2  # every reply is primed with <im_start>assistant
+        return num_tokens
+    else:
+        raise NotImplementedError(f"""num_tokens_from_messages() is not presently implemented for model {model}.
+  See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens.""")
+
 
 if __name__ == '__main__':
     # Load your API key from an environment variable or secret management service
@@ -13,9 +38,12 @@ if __name__ == '__main__':
     conversation = [{"role": "system", "content": initial_prompt}]
     while True:
         message = input('> ')
+        if num_tokens_from_messages(conversation) >= 4000:
+            # TODO: compress previous messages
+            conversation = [{"role": "system", "content": initial_prompt}]
         conversation.append({"role": "user", "content": message})
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
+            model=MODEL,
             temperature=0.4,
             messages=conversation
         )
