61f778da508dc354935be190831b876145d8e62f
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-03-23 15:58:36 +0700

    Added some TODO

--- brain/LanguageProcessor.py
+++ brain/LanguageProcessor.py
@@ -1,3 +1,5 @@
+# TODO: process the answer before showing it would reduce the response time.
+
 class LanguageProcessor:
     def __init__(self, model, initial_prompt='', *args, **kwargs):
         self.model = model
