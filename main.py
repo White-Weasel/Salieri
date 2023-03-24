afaf6bea43e749cadcad5e69c9e297976295e241
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-03-24 14:44:26 +0700

    working prototype

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-import interface
-import uvicorn
+import brain
+# import interface
+# import uvicorn
+# import threading
 
-
-app = interface.app
+# app = interface.app
 
 if __name__ == '__main__':
-    port = 3000
+    # port = 3000
     # Open a ngrok tunnel to the HTTP server
     # ngrok.set_auth_token(os.getenv('ngrok_token'))
     # public_url = ngrok.connect(port)
     # print(public_url)
     # nest_asyncio.apply()=
-    uvicorn.run(app, port=port)
+    # uvicorn.run(app, port=port)
+    Salie = brain.Brain()
+    try:
+        Salie.wake_up()
+    except KeyboardInterrupt:
+        Salie.sleep()
