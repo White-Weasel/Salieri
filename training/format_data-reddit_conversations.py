43835f3cc9579a53837f1864740cd248febbb1eb
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-05-11 17:30:31 +0700

    added reddit crawler and update reddit data formater

--- training/format_data-reddit_conversations.py
+++ training/format_data-reddit_conversations.py
@@ -11,12 +11,9 @@ def main():
     }
     raw_data = pandas.read_csv("/home/giang/data/reddit dataset/casual_data_windows.csv",
                                dtype=dtypes, keep_default_na=False, na_values=['NaN'])
-    raw_data = [line[1:] for line in raw_data.values]
-    # remove /s, subreddit, user mentions, ...
-    filtered = ['/r/', 'r/', '\\r\\', 'r\\', 'u/', '/u/', '\\u\\', 'u\\', '/s', ' op ', ' op,', ' op.']
-    data = [line for line in raw_data if not any(x in ''.join(line).lower() for x in filtered)]
+    raw_data = [line[1:] for line in raw_data.values]  # remove /s, subreddit, user mentions, ...
     # remove emoji (?)
-    data = [[emoji.replace_emoji(value, '') for value in line] for line in data]
+    data = [[emoji.replace_emoji(value, '') for value in line] for line in raw_data[:20000]][:10000]
 
     all_conversations = []
 
@@ -46,13 +43,15 @@ def main():
         else:
             branch = find_branches(line[:1])
             if branch:
-                new_branch = all_conversations[branch[0]].copy()
+                new_branch = all_conversations[branch[0]][:1].copy()
                 new_branch += line[1:]
                 all_conversations.append(new_branch)
             else:
                 all_conversations.append(line)
         print(f"done {index + 1} lines")
 
+    filtered = ['/r/', 'r/', '\\r\\', 'r\\', 'u/', '/u/', '\\u\\', 'u\\', '/s', ' op ', ' op,', ' op.']
+    all_conversations = [line for line in all_conversations if not any(x in ''.join(line).lower() for x in filtered)]
     pass
 
 
