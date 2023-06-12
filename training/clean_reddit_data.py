c4ba48a96b790e8f5701e8de713768778329305d
Binh Giang <NguyenDanhBinhGiang@gmail.com>
2023-06-12 17:34:38 +0700

    update cleaning reddit data

--- training/clean_reddit_data.py
+++ training/clean_reddit_data.py
@@ -1,7 +1,12 @@
 import json
 import re
+import uuid
 
 import emoji
+from datasets import Dataset
+
+PROMPT_END_TOKEN = r"<|endofprompt|>"
+TEXT_END_TOKEN = r"<|endoftext|>"
 
 # f = open("/home/giang/data/rd_dataset/rddit.json", 'r')
 # a = json.load(f)
@@ -18,7 +23,8 @@ filter_words = ['/r/', 'r/', '\\r\\', 'r\\',
                 ' op ', ' op,', ' op.',
                 'https://', 'http://',
                 '\nedit:',
-                "\*"
+                "\*",
+                r'`', '▽', '♡', 'ᴗ', '´', ':)', r"_/¯",
                 ]
 remove_words = ['/s', '\s',
                 '^',
@@ -43,24 +49,59 @@ reformat_keywords = [
     '`?show=all`'
 ]
 
-all_conversations = [conver for conver in a if not any(x in ''.join(conver).lower() for x in filter_words)]
-for conver in all_conversations:
-    for line in conver:
+all_conversations = [conver for conver in a
+                     if not any(x in line[line.index(':') + 1:].lower()
+                                for line in conver
+                                for x in filter_words
+                                )
+                     ]
+for c_index, conver in enumerate(all_conversations):
+    for l_index, line in enumerate(conver):
+        header = line[:line.index(': ') + 2]
+        content = line[line.index(': ') + 2:]
         for remove_w in remove_words:
-            if remove_w in line.lower():
-                line = line[:line.index(remove_w)] + line[line.index(remove_w) + len(remove_w):]
+            if remove_w in content.lower():
+                try:
+                    content = content[:line.index(remove_w)] + content[content.index(remove_w) + len(remove_w):]
+                except ValueError:
+                    pass
 
+        # # replace "abc" with abc
+        # content = re.sub(r"(\"+)([^*\n]*)(\"+)", r"\2", content)
+        # # replace “abc” with abc
+        # content = re.sub(r"(\"+)([^*\n]*)(\"+)", r"\2", content)
+        # # replace 'abc' with abc
+        # content = re.sub(r"(\'+)([^*\n]*)(\'+)", r"\2", content)
         # replace **abc** with abc
-        line = re.sub(r"(\*+)([^*\n]*)(\*+)", r"\2", line)
+        content = re.sub(r"(\*+)([^*\n]*)(\*+)", r"\2", content)
         # replace ~~abc~~ with abc
-        line = re.sub(r"(~+)([^~\n]*)(~+)", r"\2", line)
+        content = re.sub(r"(~+)([^~\n]*)(~+)", r"\2", content)
         # replace >!abc!< with abc
-        line = re.sub(r"(>!)([^!<\n]*)(!<+)", r"\2", line)
+        content = re.sub(r"(>!)([^!<\n]*)(!<+)", r"\2", content)
         # replace ^(abc) with abc
-        line = re.sub(r"(\^\()([^^\(\)\n]*)(\))", r"\2", line)
+        content = re.sub(r"(\^\()([^^\(\)\n]*)(\))", r"\2", content)
         # replace ^abc ^def with abc def
-        line = re.sub(r"(\^)([^\^\n\b]*)", r"\2", line)
+        content = re.sub(r"(\^)([^\^\n\b]*)", r"\2", content)
         # replace >abc \n\n>def with abc\n\ndef
-        line = re.sub(r"^(>)(.*)", r"\2", line)
+        content = re.sub(r"^(>)(.*)", r"\2", content)
+        line = header + content
+        conver[l_index] = line
+    all_conversations[c_index] = conver
 
+ds = {
+    'instruction': [],
+    'input': [],
+    'output': [],
+}
+for conver in all_conversations[:500]:
+    instruction_ = 'As Salieri, response to the following conversation:'
+    input_ = '\n'.join([line.replace('/u/', '@') for line in conver[:-1]]) + f"\n@Salieri: "
+    output_ = conver[-1][conver[-1].index(':') + 1:].strip()
+    # id_ = str(uuid.uuid1())
+    # messages = '\n'.join(conver[:-1]) + f"\n{conver[-1][:conver[-1].index(':') + 1]} {PROMPT_END_TOKEN}{conver[-1][conver[-1].index(':') + 1:].strip()}{TEXT_END_TOKEN}"
+    ds['instruction'].append(instruction_)
+    ds['input'].append(input_)
+    ds['output'].append(output_)
+dataset = Dataset.from_dict(ds)
+dataset.push_to_hub("binhgiangnguyendanh/reddit_casual_conversation_for_alpaca_lora-TEST", private=True)
 pass
